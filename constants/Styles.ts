7cad895b86f0948678cb2f4764246a1834f2e667
imHeeshan <imHeeshan@gmail.com>
2024-09-10 21:16:55 +0530

    created onboarding screen and initial routing

---
+++ constants/Styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const defaultStyles = StyleSheet.create({
+    pillButton: {
+        // padding: 10,
+        height: 50,
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
